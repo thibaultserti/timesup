b4ecdf231ccb5ad4a652635c9d028b660f930fc6
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 15:00:35 +0200

    Add styles

--- index.php
+++ index.php
@@ -10,36 +10,38 @@
 </head>
 
 <body>
-    <img src="/img/timesup.png" alt="logo timesup">
-    <div class="box">
-        <form action="">
-            <input type="text" placeholder="pseudo">
-            <select name="" id="">
-                <option value="animal">Animal</option>
-                <option value="object"> Objet</option>
-                <option value="actor">Acteur.trices</option>
-            </select>
-            <div class="box">
-                <img src="img/arrow.svg" alt="arrow">
-                <img src="/img/avatars/horse.svg" alt="avatar">
-                <img src="img/arrow.svg" alt="arrow">
-            </div>
-            <button>Jouer</button>
-            <button>Créér une partie</button>
-        </form>
-    </div>
-    <div class="box">
-        <h1>News</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
-        <h1>À propos</h1>
+    <header>
+        <img src="/img/timesup.png" alt="logo timesup" id="logo"></header>
+    <section>
+        <div class="box box-container" >
+            <form action="">
+                <input type="text" placeholder="pseudo">
+                <select name="" id="">
+                    <option value="animal">Animal</option>
+                    <option value="object"> Objet</option>
+                    <option value="actor">Acteur.trices</option>
+                </select>
+                <div class="box" id="avatars-container">
+                    <img src="img/arrow.svg" alt="arrow" class="arrow" id="arrow1">
+                    <img src="/img/avatars/horse.svg" alt="avatar" class="avatar">
+                    <img src="img/arrow.svg" alt="arrow" class="arrow">
+                </div>
+                <button>Jouer</button>
+                <button>Créér une partie</button>
+            </form>
+        </div>
+        <div class="box box-container">
+            <h1>News</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
+            <h1>À propos</h1>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>
-        <h1> Règles du jeu </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>
+            <h1> Règles du jeu </h1>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aperiam a obcaecati, ducimus tempore facere quas sapiente cupiditate neque iure iusto blanditiis quis dolor at! Culpa quasi sequi asperiores itaque!
-        </p>
-    </div>
-
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aperiam a obcaecati, ducimus tempore facere quas sapiente cupiditate neque iure iusto blanditiis quis dolor at! Culpa quasi sequi asperiores itaque!
+            </p>
+        </div>
+    </section>
 </body>
 
 </html>

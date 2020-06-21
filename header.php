1c3054450e68fb671eb499e4e71a1163f3ca29f1
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 18:21:59 +0200

    Update structure

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <title>Time's Up</title>
+
+</head>
+
+<body>
+    <img src="/img/timesup.png" alt="logo timesup" id="logo">

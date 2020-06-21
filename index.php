9fbbea302361bce5a087427a464628f386762ea6
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 14:13:42 +0200

    Add home page structure

--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <title>Time's Up</title>
+
 </head>
+
 <body>
-    
+    <img src="/img/timesup.png" alt="logo timesup">
+    <div class="box">
+        <form action="">
+            <input type="text" placeholder="pseudo">
+            <select name="" id="">
+                <option value="animal">Animal</option>
+                <option value="object"> Objet</option>
+                <option value="actor">Acteur.trices</option>
+            </select>
+            <div class="box">
+                <img src="img/arrow.svg" alt="arrow">
+                <img src="/img/avatars/horse.svg" alt="avatar">
+                <img src="img/arrow.svg" alt="arrow">
+            </div>
+            <button>Jouer</button>
+            <button>Créér une partie</button>
+        </form>
+    </div>
+    <div class="box">
+        <h1>News</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
+        <h1>À propos</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>
+        <h1> Règles du jeu </h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aperiam a obcaecati, ducimus tempore facere quas sapiente cupiditate neque iure iusto blanditiis quis dolor at! Culpa quasi sequi asperiores itaque!
+        </p>
+    </div>
+
 </body>
+
 </html>

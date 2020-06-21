16da9ea68602f64e62dab41f46fb386e85c92849
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 18:39:24 +0200

    Add setup game page

--- setup.php
+++ setup.php
@@ -1,7 +1,33 @@
 <?php
 require_once 'header.php'
 ?>
+<section>
+    <div class="box">
+        <form action="" class="box-container">
+            <select name="" id="">
 
+                <option value="100">100 points</option>
+                <option value="90">90 points</option>
+                <option value="80">80 points</option>
+            </select>
+            <select name="" id="">
+
+                <option value="30">30s</option>
+                <option value="25">25s</option>
+                <option value="20">20s</option>
+            </select>
+            <textarea name="" id="" cols="30" rows="10" placeholder="Description de la partie">
+
+        </textarea>
+            <button>Lancer la partie</button>
+        </form>
+    </div>
+    <div class="box">
+        <div class="box-container">
+
+        </div>
+    </div>
+</section>
 <?php
 require_once 'footer.php'
 ?>

6c2b1c77f566d2f0d7e6b718aa6a84e73b5387e4
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 17:58:20 +0200

    Add favicon and clik handling on news

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <title>Time's Up</title>
 
 </head>
@@ -14,32 +15,32 @@
     <section>
         <div class="box">
             <form action="" class="box-container">
-                    <input type="text" placeholder="Pseudo">
-                    <select name="" id="">
+                <input type="text" placeholder="Pseudo">
+                <select name="" id="">
 
-                        <option value="animal">Animal</option>
-                        <option value="object"> Objet</option>
-                        <option value="actor">Acteur.trices</option>
-                    </select>
+                    <option value="animal">Animaux</option>
+                    <option value="object"> Objets</option>
+                    <option value="actor">Acteur.trices</option>
+                </select>
 
-                    <div class="box" id="avatars-container">
-                        <img src="img/arrow.svg" alt="arrow" class="arrow" id="arrow1">
-                        <img src="/img/avatars/horse.svg" alt="avatar" class="avatar">
-                        <img src="img/arrow.svg" alt="arrow" class="arrow">
-                    </div>
-                    <button>Jouer</button>
-                    <button>Créér une partie</button>
+                <div class="box" id="avatars-container">
+                    <img src="img/arrow.svg" alt="arrow" class="arrow" id="arrow1">
+                    <img src="/img/avatars/horse.svg" alt="avatar" class="avatar">
+                    <img src="img/arrow.svg" alt="arrow" class="arrow">
+                </div>
+                <button>Jouer</button>
+                <button>Créér une partie</button>
             </form>
         </div>
         <div class="box box-container">
-            <h1>News</h1>
+            <h1><a href="#news">News</a></h1>
             <p id="news">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
-            <h1>À propos</h1>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>
-            <h1> Règles du jeu </h1>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aperiam a obcaecati, ducimus tempore facere quas sapiente cupiditate neque iure iusto blanditiis quis dolor at! Culpa quasi sequi asperiores itaque!
+            
+            <h1><a href="#apropos">À propos</a></h1>
+            <p id="apropos">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>
+            
+            <h1><a href="#rules">Règles du jeu</a> </h1>
+            <p id="rules">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, aperiam a obcaecati, ducimus tempore facere quas sapiente cupiditate neque iure iusto blanditiis quis dolor at! Culpa quasi sequi asperiores itaque!
             </p>
         </div>
     </section>

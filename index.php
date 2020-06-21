d9c339ff23facfe34833783120dd298e0781d44e
Thibault Serti <thibault.serti@gmail.com>
2020-06-21 17:37:07 +0200

    Improve style

--- index.php
+++ index.php
@@ -10,27 +10,30 @@
 </head>
 
 <body>
-        <img src="/img/timesup.png" alt="logo timesup" id="logo">
-        <div class="box box-container" >
-            <form action="">
-                <input type="text" placeholder="pseudo">
-                <select name="" id="">
-                    <option value="animal">Animal</option>
-                    <option value="object"> Objet</option>
-                    <option value="actor">Acteur.trices</option>
-                </select>
-                <div class="box" id="avatars-container">
-                    <img src="img/arrow.svg" alt="arrow" class="arrow" id="arrow1">
-                    <img src="/img/avatars/horse.svg" alt="avatar" class="avatar">
-                    <img src="img/arrow.svg" alt="arrow" class="arrow">
-                </div>
-                <button>Jouer</button>
-                <button>Créér une partie</button>
+    <img src="/img/timesup.png" alt="logo timesup" id="logo">
+    <section>
+        <div class="box">
+            <form action="" class="box-container">
+                    <input type="text" placeholder="Pseudo">
+                    <select name="" id="">
+
+                        <option value="animal">Animal</option>
+                        <option value="object"> Objet</option>
+                        <option value="actor">Acteur.trices</option>
+                    </select>
+
+                    <div class="box" id="avatars-container">
+                        <img src="img/arrow.svg" alt="arrow" class="arrow" id="arrow1">
+                        <img src="/img/avatars/horse.svg" alt="avatar" class="avatar">
+                        <img src="img/arrow.svg" alt="arrow" class="arrow">
+                    </div>
+                    <button>Jouer</button>
+                    <button>Créér une partie</button>
             </form>
         </div>
         <div class="box box-container">
             <h1>News</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
+            <p id="news">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, error atque? Laboriosam aspernatur culpa expedita ea, eveniet at accusamus, hic corporis dolor quis excepturi animi, distinctio possimus quisquam rerum nisi.</p>
             <h1>À propos</h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eius architecto nihil dolores quos dolor. Est ex praesentium minima saepe, minus quaerat, hic molestiae quam earum dolore vel. Soluta, dolores!</p>

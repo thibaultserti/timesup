e0d07fc6be249c439845fb13f593640f893b1aeb
Javisty <aymec.78620@gmail.com>
2020-06-23 15:37:08 +0200

    Put all the library files in one specific directory, for cleaner structure

---
+++ SQLlibs/lib.sql.php
@@ -0,0 +1,40 @@
+<?php
+
+// create user in database
+function createUser($pseudo, $avatar)
+{
+    
+}
+
+// update the game room for an user
+function enterGame($user_id, $game_id)
+{
+    
+}
+
+// undefine game room for an user
+function leaveGame($user_id)
+{
+    
+}
+
+// give points earned to user
+// TODO : control if the game ends
+function givePoints($user_id)
+{
+    
+}
+
+// create a game room
+function createRoom($manager_id,$points_max, $duration, $categorie)
+{
+    
+}
+
+// update which word is being played in a game
+function updateWord($game_id, $word_id)
+{
+    
+}
+
+?>

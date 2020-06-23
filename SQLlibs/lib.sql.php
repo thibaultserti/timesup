30eb2e927d1a3a102ba196ac9d2c9344aade81bc
Javisty <aymec.78620@gmail.com>
2020-06-23 17:27:18 +0200

    Complete functions in lib.sql.php (+ effectively delete files, according to the previous structural change)

--- SQLlibs/lib.sql.php
+++ SQLlibs/lib.sql.php
@@ -1,40 +1,88 @@
 <?php
 
+include_once("./maLibSQL.pdo.php");
+
 // create user in database
 function createUser($pseudo, $avatar)
 {
-    
+    $sql = "INSERT INTO User (pseudo, avatar) VALUES ('$pseudo', '$avatar')";
+    return SQLInsert($sql);
 }
 
 // update the game room for an user
 function enterGame($user_id, $game_id)
 {
-    
+    $sql = "UPDATE User SET idGame = '$game_id' WHERE id = '$user_id'";
+    return SQLUpdate($sql);
 }
 
 // undefine game room for an user
 function leaveGame($user_id)
 {
-    
+    $sql = "UPDATE User SET idGame = NULL WHERE id = '$user_id'";
+    return SQLUpdate($sql);
 }
 
-// give points earned to user
-// TODO : control if the game ends
-function givePoints($user_id)
+/**
+ * give points earned to user,
+ * @return boolean|int true when game limit is exceeded, the new score elsewise
+ */
+function givePoints($user_id, $points)
 {
-    
+    $current_score = getValue_("User", "points", "id", $user_id);
+    if (!is_null($current_score)) {
+        $new_score = $current_score + $points;
+    } else {
+        $new_score = $points;
+    }
+    $sql = "UPDATE User SET points = '$new_score' WHERE id = '$user_id'";
+    SQLUpdate($sql);
+
+    $game_id = getValue_("User", "idGame", "id", $user_id);
+    $game_limit = getValue_("Game", "pointsLimit", "id", $game_id);
+
+    if ($new_score >= $game_limit) {
+        // game ends here
+        return true;
+    } else {
+        return $new_score;
+    }
 }
 
 // create a game room
-function createRoom($manager_id,$points_max, $duration, $categorie)
+function createRoom($manager_id,$points_max, $duration, $categorie, $token)
 {
-    
+    $sql = "INSERT INTO Game (categorie, duration, pointsLimit, token)
+            VALUES ('$categorie', '$duration', '$points_max', '$token')";
+    $res = SQLInsert($sql);
+    setValue_("User", "manager", 1, "id", $manager_id);
+    return $res;
 }
 
 // update which word is being played in a game
 function updateWord($game_id, $word_id)
 {
-    
+    return setValue_("Game", "idWord", $word_id, "id", $game_id);
+}
+
+
+
+/**
+ * Get the value from $table.$column where $column_cond = $value_cond
+ */
+function getValue_($table, $column, $column_cond, $value_cond)
+{
+    $sql = "SELECT '$column' FROM '$table' WHERE '$column_cond' = '$value_cond'";
+    return SQLGetChamp($sql);
+}
+
+/**
+ * Set the value from $table.$column where $column_cond = $value_cond on $value
+ */
+function setValue_($table, $column, $value, $column_cond, $value_cond)
+{
+    $sql = "UPDATE '$table' SET '$column' = '$value' WHERE '$column_cond' = '$value_cond'";
+    return SQLUpdate($sql);
 }
 
 ?>

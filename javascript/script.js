71fb9a13955ac79080976a2fab79a625c22600d6
Ashka <nino.segala@hotmail.fr>
2020-06-25 10:33:22 +0200

    Delete the avatars 'dog2', 'mouse' and 'rabbit'

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,10 @@
 function changeAvatar(arrow) {
     console.log("change the avatar");
     //"endBoucle" est utilisé pour transformé 'zebra' en 'bear'
-    //suppression de 'dog2' (identique à 'dog') car cela pose des problèmes (dog2 -> deer2)
-    var avatarListRight = ["endBoucle", "zebra", "racoon", "rabbit", "pig", "panda", "mouse", 
+    var avatarListRight = ["endBoucle", "zebra", "racoon", "pig", "panda", 
     "monkey", "horse", "fox", "elephant", "dog", "deer", "cat", "beaver", "bear", "endBoucle"];
-    var avatarListLeft = ["endBoucle", "zebra", "bear", "beaver", "cat", "deer", "dog",
-    "elephant", "fox", "horse", "monkey", "mouse", "panda", "pig", "rabbit", "racoon", "endBoucle"];
+    var avatarListLeft = ["endBoucle", "bear", "beaver", "cat", "deer", "dog",
+    "elephant", "fox", "horse", "monkey", "panda", "pig", "racoon", "zebra", "endBoucle"];
     var avatarList = avatarListRight;
     if (arrow.id=="arrow1") {
         var avatarList = avatarListLeft;

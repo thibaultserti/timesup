8b5583503125c4e754f912af84cac599888ccd95
Ashka <nino.segala@hotmail.fr>
2020-06-25 10:57:05 +0200

    Add comments to script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,8 @@
-function changeAvatar(arrow) {
+//On change l'avatar choisi lorsqu'on clique sur les flèches gauche ou droite
+function changeAvatar(arrow) { //pour index.php
     console.log("change the avatar");
-    //"endBoucle" est utilisé pour transformé 'zebra' en 'bear'
+    //avatarListRight est pour la flèche droite et avatarListLeft pour la flèche gauche
+    //"endBoucle" est utilisé pour transformé 'zebra' en 'bear' (pour régler les problèmes de cycle)
     var avatarListRight = ["endBoucle", "zebra", "racoon", "pig", "panda", 
     "monkey", "horse", "fox", "elephant", "dog", "deer", "cat", "beaver", "bear", "endBoucle"];
     var avatarListLeft = ["endBoucle", "bear", "beaver", "cat", "deer", "dog",
@@ -10,7 +12,7 @@ function changeAvatar(arrow) {
         var avatarList = avatarListLeft;
     }
     for (var i=0; i<avatarList.length; i++) {
+        //on essaye tous les changements possibles, seul celui qui nous intéresse est effectué
         document.getElementById("avatar").src = avatar.src.replace(avatarList[i+1], avatarList[i]);
     }
-
 }
